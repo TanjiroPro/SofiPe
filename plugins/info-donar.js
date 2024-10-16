41ef4481d0eb9ce6a54e08b0e84b2cf2a48d01fb
TanjiroPro <Yallico2024@gmail.com>
2024-10-16 16:54:11 -0500

    iniciado

---
+++ plugins/info-donar.js
@@ -0,0 +1,39 @@
+
+let handler = async (m, { conn, command, usedPrefix }) => {
+let pp = 'https://qu.ax/UTgs.jpg'
+let name = await conn.getName(m.sender)
+let _uptime = process.uptime() * 1000
+let _muptime
+if (process.send) { process.send('uptime')
+_muptime = await new Promise(resolve => { process.once('message', resolve) 
+setTimeout(resolve, 1000) }) * 1000}
+let uptime = clockString(_uptime)
+let estado = `
+💫 Creador 
+• Yallico
+
+ 💫 Activo 
+• ${uptime}
+
+ 💫 Bot 
+• MickeyBot-Md
+
+ 💫 Donar 
+• Puedes apoyar el bot donando al PayPal
+
+ 💫 Paypal 
+• https://paypal.me/YallicoPe?country.x=PE&locale.x=es_XC`
+await conn.sendButton(m.chat, estado, ' ', pp, [
+['🤴 Dueño 🤴', '.owner'], ['🫀 Grupos 🫀', '/grupos']], null, [['💫 Canal 💫', `${canal}`]], estilo)
+}
+handler.help = ['donate']
+handler.tags = ['main']
+handler.command = /^(apoyar|donate|donar?)$/i
+
+export default handler
+
+function clockString(ms) {
+let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
